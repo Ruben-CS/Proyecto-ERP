037079f4929e80deb5997ab961b35a3d1d8e5991
Ruben Ferreira <rubend192000@gmail.com>
2023-03-02 22:50:14 -0400

    Agregado el modelo de usuario

--- ModuloContabilidadApi/Models/Usuario.cs
+++ ModuloContabilidadApi/Models/Usuario.cs
@@ -1,6 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
 namespace ModuloContabilidadApi.Models;
 
-public class Usuario
+public class Usuario : IdentityUser
 {
+    [Key] public string IdUsuario { get; set; }
+    public       string Nombre    { get; set; }
+    public       string Password  { get; set; }
     
 }
