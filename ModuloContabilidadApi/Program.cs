b5f6d060d59e636d97d698fe0c7e50af076a4380
Ruben Ferreira <rubend192000@gmail.com>
2023-03-06 10:15:36 -0400

    Borre los data seeders que no servian

--- ModuloContabilidadApi/Program.cs
+++ ModuloContabilidadApi/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ModuloContabilidadApi.ApplicationContexts;
-using ModuloContabilidadApi.Data;
-using ModuloContabilidadApi.Data.Interfaces;
 using ModuloContabilidadApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,8 +15,6 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddIdentity<Usuario, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
-
-builder.Services.AddScoped<IDataSeeder, AdminUserSeeder>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(o =>
 {
@@ -45,21 +41,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-using (var scope = app.Services.CreateScope())
-{
-    try
-    {
-        var dataSeeder =
-            scope.ServiceProvider.GetRequiredService<IDataSeeder>();
-
-        dataSeeder.SeedAsync().Wait();
-    }
-    catch (Exception ex)
-    {
-        var logger = app.Services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred while seeding the database.");
-    }
-}
 
 app.UseHttpsRedirection();
 
