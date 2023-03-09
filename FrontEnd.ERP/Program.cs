906c4ca4dfd11be2d2051c28556a0a683c158b22
Ruben Ferreira <rubend192000@gmail.com>
2023-03-09 10:20:08 -0400

    Cambios?

--- FrontEnd.ERP/Program.cs
+++ FrontEnd.ERP/Program.cs
@@ -1,6 +1,7 @@
 using FrontEnd.ERP.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using ModuloContabilidadApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+
 
 var app = builder.Build();
 
