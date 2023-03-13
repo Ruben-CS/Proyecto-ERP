aea6e5158027bb87ce603fbb6bfb946c2625f24f
Ruben Ferreira <rubend192000@gmail.com>
2023-03-12 23:03:13 -0400

    Editado el dto de empresa

--- ModuloContabilidadApi/Models/Dtos/EmpresaDto.cs
+++ ModuloContabilidadApi/Models/Dtos/EmpresaDto.cs
@@ -5,15 +5,25 @@ namespace ModuloContabilidadApi.Models.Dtos;
 
 public class EmpresaDto
 {
-    public int            IdEmpresa { get; set; }
-    public string         Nombre    { get; set; }
-    public string         Nit       { get; set; }
-    public string         Sigla     { get; set; }
-    public string?        Telefono  { get; set; }
-    public string         Correo    { get; set; }
-    public string?        Direccion { get; set; }
-    public int            Niveles   { get; set; }
-    public bool           IsDeleted { get; set; } = false;
+    public int IdEmpresa { get; set; }
+
+    [Required(ErrorMessage = "Empresa requiere un nombre")]
+    string Nombre { get; set; }
+
+    [Required(ErrorMessage = "Empresa requiere un NIT")]
+    public string Nit { get; set; }
+
+    [Required(ErrorMessage = "Empresa requiere una sigla")]
+
+    public string Sigla { get; set; }
+
+    public string? Telefono  { get; set; }
+    [EmailAddress]
+    public string  Correo    { get; set; }
+    public string? Direccion { get; set; }
+    public int     Niveles   { get; set; }
+    public bool    IsDeleted { get; set; } = false;
+
     public List<Gestion>? Gestiones { get; set; }
     // public byte[]         TimeStamp { get; set; }
 
