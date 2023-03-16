90d9c2a40e4b8466cfff536be93c417924eddb76
Ruben Ferreira <rubend192000@gmail.com>
2023-03-16 13:31:09 -0400

    Login funciona

--- NET7.BlazorServerApp/Program.cs
+++ NET7.BlazorServerApp/Program.cs
@@ -1,23 +1,20 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using NET7.BlazorServerApp.Data;
-using BlazorBootstrap; // Add this line
+using BlazorBootstrap;
+using System.Net.Http.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddBlazorBootstrap(); // Add this line
+builder.Services.AddBlazorBootstrap();
+builder.Services.AddScoped<HttpClient>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -26,6 +23,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthorization();
+app.UseAuthentication();
+
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
