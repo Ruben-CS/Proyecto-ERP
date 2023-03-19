540d5f5d5a5ff7a50ca8bf30c9cde708d7251363
Ruben Ferreira <rubend192000@gmail.com>
2023-03-18 21:59:28 -0400

    Cambios en el front

--- NET7.BlazorServerApp/Program.cs
+++ NET7.BlazorServerApp/Program.cs
@@ -11,12 +11,13 @@ builder.Services.AddScoped<HttpClient>();
 
 var app = builder.Build();
 
+
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
