50868dee0d633e3250db95c4b2b391f4adf4bfe7
Ruben Ferreira <rubend192000@gmail.com>
2023-04-12 01:18:21 -0400

    minor changes

--- BlazorFrontend/Pages/Empresa/Inicio.razor.cs
+++ BlazorFrontend/Pages/Empresa/Inicio.razor.cs
@@ -1,151 +1,134 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
-using System.Net.Http;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Web.Virtualization;
-using Microsoft.JSInterop;
-using BlazorFrontend;
-using BlazorFrontend.Shared;
 using MudBlazor;
-using System.Net.Http.Json;
 using Modelos.Models.Dtos;
 using BlazorFrontend.Pages.Empresa.Crear;
 using BlazorFrontend.Pages.Empresa.Editar;
 using BlazorFrontend.Pages.Empresa.Eliminar;
-using BlazorFrontend.Services;
 using Microsoft.AspNetCore.Http.Extensions;
-using Variant =  MudBlazor . Variant ;
 using DialogOptions =  MudBlazor . DialogOptions ;
 
-namespace BlazorFrontend.Pages.Empresa
+namespace BlazorFrontend.Pages.Empresa;
+
+public partial class Inicio
 {
-    public partial class Inicio
+    [Inject]
+    ISnackbar Snackbar { get; set; } = null !;
+    private IEnumerable<EmpresaDto> _empresas = new List<EmpresaDto>();
+    private int                     SelectedEmpresaId { get; set; }
+
+    private async Task LoadSelectedEmpresaAsync(int selectedId)
     {
-        [Inject]
-        ISnackbar Snackbar { get; set; } = null !;
-        private IEnumerable<EmpresaDto> _empresas = new List<EmpresaDto>();
-        private int SelectedEmpresaId { get; set; }
+        SelectedEmpresaId = selectedId;
+        await InvokeAsync(StateHasChanged);
+    }
 
-        private async Task LoadSelectedEmpresaAsync(int selectedId)
+    private async Task ShowMudCrearEmpresaModal()
+    {
+        var options = new DialogOptions
         {
-            SelectedEmpresaId = selectedId;
-            await InvokeAsync(StateHasChanged);
-        }
-
-        private async Task ShowMudCrearEmpresaModal()
+            CloseOnEscapeKey = true,
+            MaxWidth         = MaxWidth.Small,
+            FullWidth        = true
+        };
+        var parameters = new DialogParameters
         {
-            var options = new DialogOptions
             {
-                CloseOnEscapeKey = true,
-                MaxWidth = MaxWidth.Small,
-                FullWidth = true
-            };
-            var parameters = new DialogParameters
-            {
-                {
-                    "OnEmpresaAdded",
-                    EventCallback.Factory.Create<EmpresaDto>(this, OnEmpresaAddedHandler)
-                }
-            };
-            await DialogService.ShowAsync<CrearEmpresa>("Llene los datos de la empresa", parameters, options);
-        }
-
-        //todo refactor this duplicate method
-        private async Task OnEmpresaAddedHandler(EmpresaDto addedEmpresa)
-        {
-            _empresas = await EmpresaService.GetEmpresasAsync();
-            SelectedEmpresaId = addedEmpresa.IdEmpresa;
-            await LoadSelectedEmpresaAsync(SelectedEmpresaId);
-        }
-
-        private async Task OnEmpresaDeletedHandler(EmpresaDto deletedEmpresa)
-        {
-            _empresas = await EmpresaService.GetEmpresasAsync();
-            SelectedEmpresaId = deletedEmpresa.IdEmpresa;
-            await LoadSelectedEmpresaAsync(SelectedEmpresaId);
-        }
-
-        protected override async Task OnInitializedAsync()
-        {
-            _empresas = await EmpresaService.GetEmpresasAsync();
-            StateHasChanged();
-        }
-
-        private async Task Editar(MouseEventArgs obj)
-        {
-            var options = new DialogOptions
-            {
-                CloseOnEscapeKey = true,
-                MaxWidth = MaxWidth.Small,
-                FullWidth = true,
-            };
-            var parameters = new DialogParameters
-            {
-                {
-                    "SelectedEmpresaId",
-                    SelectedEmpresaId
-                }
-            };
-            await DialogService.ShowAsync<EditarEmpresa>("Edite los datos de la empresa", parameters, options);
-        }
-
-        private async void Eliminar(MouseEventArgs obj)
-        {
-            var options = new DialogOptions
-            {
-                CloseOnEscapeKey = true,
-                MaxWidth = MaxWidth.Small,
-                FullWidth = true,
-            };
-            var parameters = new DialogParameters
-            {
-                {
-                    "SelectedEmpresaId",
-                    SelectedEmpresaId
-                },
-                {
-                    "OnEmpresaDeleted",
-                    EventCallback.Factory.Create<EmpresaDto>(this, OnEmpresaDeletedHandler)
-                }
-            };
-            await DialogService.ShowAsync<EliminarEmpresa>("Edite los datos de la empresa", parameters, options);
-        }
-
-        private void NavigateToPage()
-        {
-            if (SelectedEmpresaId != default)
-            {
-                var uri = NavigationManager.ToAbsoluteUri("inicio/overview");
-                var queryBuilder = new QueryBuilder
-                {
-                    {
-                        "id",
-                        SelectedEmpresaId.ToString()
-                    }
-                };
-                uri = new UriBuilder(uri)
-                {
-                    Query = queryBuilder.ToString()
-                }.Uri;
-                NavigationManager.NavigateTo(uri.ToString());
+                "OnEmpresaAdded",
+                EventCallback.Factory.Create<EmpresaDto>(this, OnEmpresaAddedHandler)
             }
-            else
-            {
-                Snackbar.Add("Seleccione una empresa antes de continuar.", Severity.Info);
-            }
-        }
+        };
+        await DialogService.ShowAsync<CrearEmpresa>("Llene los datos de la empresa", parameters, options);
+    }
 
-        private async Task CerrarSesion()
+    //todo refactor this duplicate method
+    private async Task OnEmpresaAddedHandler(EmpresaDto addedEmpresa)
+    {
+        _empresas         = await EmpresaService.GetEmpresasAsync();
+        SelectedEmpresaId = addedEmpresa.IdEmpresa;
+        await LoadSelectedEmpresaAsync(SelectedEmpresaId);
+    }
+
+    private async Task OnEmpresaDeletedHandler(EmpresaDto deletedEmpresa)
+    {
+        _empresas         = await EmpresaService.GetEmpresasAsync();
+        SelectedEmpresaId = deletedEmpresa.IdEmpresa;
+        await LoadSelectedEmpresaAsync(SelectedEmpresaId);
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        _empresas = await EmpresaService.GetEmpresasAsync();
+        StateHasChanged();
+    }
+
+    private async Task Editar(MouseEventArgs obj)
+    {
+        var options = new DialogOptions
         {
-            await Task.Delay(2500);
-            NavigationManager.NavigateTo("/");
+            CloseOnEscapeKey = true,
+            MaxWidth         = MaxWidth.Small,
+            FullWidth        = true,
+        };
+        var parameters = new DialogParameters
+        {
+            {
+                "SelectedEmpresaId",
+                SelectedEmpresaId
+            }
+        };
+        await DialogService.ShowAsync<EditarEmpresa>("Edite los datos de la empresa", parameters, options);
+    }
+
+    private async void Eliminar(MouseEventArgs obj)
+    {
+        var options = new DialogOptions
+        {
+            CloseOnEscapeKey = true,
+            MaxWidth         = MaxWidth.Small,
+            FullWidth        = true,
+        };
+        var parameters = new DialogParameters
+        {
+            {
+                "SelectedEmpresaId",
+                SelectedEmpresaId
+            },
+            {
+                "OnEmpresaDeleted",
+                EventCallback.Factory.Create<EmpresaDto>(this, OnEmpresaDeletedHandler)
+            }
+        };
+        await DialogService.ShowAsync<EliminarEmpresa>("Edite los datos de la empresa", parameters, options);
+    }
+
+    private void NavigateToPage()
+    {
+        if (SelectedEmpresaId != default)
+        {
+            var uri = NavigationManager.ToAbsoluteUri("inicio/overview");
+            var queryBuilder = new QueryBuilder
+            {
+                {
+                    "id",
+                    SelectedEmpresaId.ToString()
+                }
+            };
+            uri = new UriBuilder(uri)
+            {
+                Query = queryBuilder.ToString()
+            }.Uri;
+            NavigationManager.NavigateTo(uri.ToString());
         }
+        else
+        {
+            Snackbar.Add("Seleccione una empresa antes de continuar.", Severity.Info);
+        }
+    }
+
+    private async Task CerrarSesion()
+    {
+        await Task.Delay(2500);
+        NavigationManager.NavigateTo("/");
     }
 }
