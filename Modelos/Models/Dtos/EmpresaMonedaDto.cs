6ac0bfe96c5e3087fd4ba892be5faf4ca60431fe
Ruben Ferreira <rubend192000@gmail.com>
2023-04-25 21:36:43 -0400

    Configure empresa modelo

--- Modelos/Models/Dtos/EmpresaMonedaDto.cs
+++ Modelos/Models/Dtos/EmpresaMonedaDto.cs
@@ -1,6 +1,31 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Modelos.Models.Enums;
+
 namespace Modelos.Models.Dtos;
 
 public class EmpresaMonedaDto
 {
-    
+    public int IdEmpresaMoneda { get; set; }
+
+    public float? Cambio { get; set; }
+
+    public EstadoEmpresaMoneda Estado { get; set; } = EstadoEmpresaMoneda.Activo;
+
+    public DateTime FechaRegistro { get; set; }
+
+    public int IdEmpresa { get; set; }
+
+    public Usuario? Usuario { get; set; }
+
+    public Empresa? Empresa { get; set; }
+
+    public int IdMonedaPrincipal { get; set; }
+
+    public int? IdMonedaAlternativa { get; set; }
+
+    public virtual Moneda? MonedaPrincipal { get; set; }
+
+    public virtual Moneda? MonedaAlternativa { get; set; }
+
+    public IEnumerable<Moneda>? Monedas { get; set; }
 }
--- Modelos/Models/EmpresaMoneda.cs
+++ Modelos/Models/EmpresaMoneda.cs
@@ -1,6 +1,38 @@
-namespace Modelos.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Modelos.Models.Enums;
+
+namespace Modelos.Models;
 
 public class EmpresaMoneda
 {
-    
+    [Key]
+    public int IdEmpresaMoneda { get; set; }
+
+    public float? Cambio { get; set; }
+
+    public EstadoEmpresaMoneda Estado { get; set; } = EstadoEmpresaMoneda.Activo;
+
+    public DateTime FechaRegistro { get; set; }
+
+    [ForeignKey("Empresa")]
+    public int IdEmpresa { get; set; }
+
+    [InverseProperty("EmpresaMonedas")]
+    public Usuario? Usuario { get; set; }
+
+    [InverseProperty("EmpresaMonedas")]
+    public Empresa? Empresa { get; set; }
+
+    [ForeignKey("MonedaPrincipal")]
+    public int IdMonedaPrincipal { get; set; }
+
+    [ForeignKey("MonedaAlternativa")]
+    public int? IdMonedaAlternativa { get; set; }
+
+    public virtual Moneda? MonedaPrincipal { get; set; }
+
+    public virtual Moneda? MonedaAlternativa { get; set; }
+
+    public IEnumerable<Moneda>? Monedas { get; set; }
 }
