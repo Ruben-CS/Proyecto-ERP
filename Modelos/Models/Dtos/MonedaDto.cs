667a4c9160d5ce4bf826df887922216a6dba945e
Ruben Ferreira <rubend192000@gmail.com>
2023-04-25 21:22:54 -0400

    implemented moneda model

--- Modelos/Models/Dtos/MonedaDto.cs
+++ Modelos/Models/Dtos/MonedaDto.cs
@@ -2,5 +2,15 @@ namespace Modelos.Models.Dtos;
 
 public class MonedaDto
 {
-    
+    public int IdMoneda { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Descripcion { get; set; } = null!;
+
+    public string Abreviatura { get; set; } = null!;
+
+    public int IdUsuario { get; set; }
+
+    public Usuario? Usuario { get; set; }
 }
