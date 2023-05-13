f4a2193ad77f45829ebdc3fa2e7fd9c4316c2090
Ruben Ferreira <rubend192000@gmail.com>
2023-05-12 23:25:50 -0400

    UI changes in inicio and comprobante dashboard

--- Services/Repository/ComprobanteRepository.cs
+++ Services/Repository/ComprobanteRepository.cs
@@ -1,9 +1,7 @@
 using AutoMapper;
-using Microsoft.EntityFrameworkCore;
 using Modelos.ApplicationContexts;
 using Modelos.Models;
 using Modelos.Models.Dtos;
-using Modelos.Models.Enums;
 using Services.Repository.Interfaces;
 
 namespace Services.Repository;
@@ -12,7 +10,6 @@ public class ComprobanteRepository : IComprobanteRepository
 {
     private readonly ApplicationDbContext _dbContext;
     private readonly IMapper              _mapper;
-
 
     public ComprobanteRepository(ApplicationDbContext dbContext, IMapper mapper)
     {
@@ -23,67 +20,10 @@ public class ComprobanteRepository : IComprobanteRepository
     public async Task<ComprobanteDto> CrearComprobante(
         ComprobanteDto comprobanteDto, int idEmpresa)
     {
-        var monedas = await _dbContext.Monedas
-                                      .Where(m => m.IdMoneda == comprobanteDto.IdMoneda)
-                                      .ToListAsync();
-        //todo nombre moneda?
-        try
-        {
-            var gestiones = await _dbContext.Gestiones.Include(g => g.Periodos)
-                                            .Where(p => p.IdEmpresa == idEmpresa)
-                                            .ToListAsync();
-            var fechaEnPeriodoAbierto = false;
-            foreach (var gestion in gestiones)
-            {
-                foreach (var periodo in gestion.Periodos)
-                {
-                    var idGestion = gestion.IdGestion;
-                    if (periodo.Estado       == EstadosPeriodo.Abierto &&
-                        comprobanteDto.Fecha >= periodo.FechaInicio    &&
-                        comprobanteDto.Fecha <= periodo.FechaFin)
-                    {
-                        if (comprobanteDto.TipoComprobante == TipoComprobante.Apertura)
-                        {
-                            var gestionActual =
-                                await _dbContext.Gestiones.FirstOrDefaultAsync(g =>
-                                    g.IdGestion == idGestion);
-                            if (gestionActual is not null)
-                            {
-                                var comprobanteApertura =
-                                    await _dbContext.Comprobantes.FirstOrDefaultAsync(c =>
-                                        c.IdEmpresa == idEmpresa && c.TipoComprobante ==
-                                        TipoComprobante.Apertura);
-                                if (comprobanteApertura is not null)
-                                {
-                                    //todo check this..
-                                }
-                            }
-                        }
-
-                        fechaEnPeriodoAbierto = true;
-                        break;
-                    }
-                }
-
-                if (fechaEnPeriodoAbierto) break;
-            }
-
-            if (!fechaEnPeriodoAbierto)
-            {
-                //throw an error
-            }
-
-            var comprobante = _mapper.Map<ComprobanteDto, Comprobante>(comprobanteDto);
-            await _dbContext.AddAsync(comprobante);
-            await _dbContext.SaveChangesAsync();
-            return await Task.FromResult(
-                _mapper.Map<Comprobante, ComprobanteDto>(comprobante));
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            throw;
-        }
+        var comprobante = _mapper.Map<ComprobanteDto, Comprobante>(comprobanteDto);
+        await _dbContext.AddAsync(comprobante);
+        await _dbContext.SaveChangesAsync();
+        return _mapper.Map<Comprobante, ComprobanteDto>(comprobante);
     }
 
     Task<IEnumerable<ComprobanteDto>> IComprobanteRepository.GetAllComprobantes(
